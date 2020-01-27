f7e58c8960c3027c947051e1d08a87827ea208c1
amelodasilva <alexis.melo_da_silva@insa-rouen.fr>
2020-01-27 16:51:14 +0100

    add(compilation): ajout fichier pour compiler

---
+++ compiler.sh
@@ -0,0 +1,2 @@
+javac -classpath ./classes -sourcepath ./src -d ./classes ./src/fr/insarouen/asi/asiaventure/*.java
+javac -classpath ./classes -sourcepath ./src -d ./classes ./src/fr/insarouen/asi/asiaventure/elements/*.java
---
+++ executer.sh
@@ -0,0 +1,2 @@
+sh compiler.sh
+java -classpath classes fr.insarouen.asi.prog.cours1.ClassMainAmpoule
