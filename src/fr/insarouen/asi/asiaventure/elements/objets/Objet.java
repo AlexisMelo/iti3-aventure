547718110aab04743fa9f317d16f0043de0f83bf
amelodasilva <alexis.melo_da_silva@insa-rouen.fr>
2020-01-29 16:08:23 +0100

    doc(elements): ajout documentation sur classes déjà présentes

--- src/fr/insarouen/asi/asiaventure/elements/objets/Objet.java
+++ src/fr/insarouen/asi/asiaventure/elements/objets/Objet.java
@@ -3,12 +3,38 @@ package fr.insarouen.asi.asiaventure.elements.objets;
 import fr.insarouen.asi.asiaventure.elements.Entite;
 import fr.insarouen.asi.asiaventure.Monde;
 
+/**
+ * Objet est une classe héritant d'Entité représentant des élements du jeu avec
+ * lesquelles le joueur pourra intéragir en les ramassant, déplaçant, utilisant, etc...
+ *
+ * @author Anna Pineau, Alexis Melo da Silva
+ *
+ * @see Entite
+*/
 public class Objet extends Entite{
 
+  /**
+   * Constructeur Objet.
+   *
+   * A la construction d'un objet, on lui attribue un monde auquel il appartient
+   * et un nom.
+   *
+   * @param nomObjet
+   *    Le nom de l'objet
+   * @param monde
+   *    Le monde auquel appartient l'objet
+   *
+   * @see Monde
+   */
   public Objet(String nomObjet, Monde monde){
     super(nomObjet,monde);
   }
 
+  /**
+   * Permet de savoir si un objet est déplaçable.
+   *
+   * @return Un booléen à vrai si l'objet est déplaçable, faux sinon.
+   */
   public boolean estDeplacable(){
     return false;
   }
--- src/fr/insarouen/asi/asiaventure/elements/structure/ElementStructurel.java
+++ src/fr/insarouen/asi/asiaventure/elements/structure/ElementStructurel.java
@@ -2,8 +2,29 @@ package fr.insarouen.asi.asiaventure.elements.structure;
 
 import fr.insarouen.asi.asiaventure.elements.Entite;
 
+/**
+ * ElementStructurel héritant d'entite qui permet de définir un élement faisant
+ * partie du décors du jeu.
+ *
+ * @author Anna Pineau, Alexis Melo da Silva
+ *
+ * @see Entite
+ */
 public class ElementStructurel extends Entite{
 
+  /**
+   * Constructeur ElementStructurel.
+   *
+   * A la construction d'un ElementStructurel, on lui attribue un monde auquel il appartient
+   * et un nom.
+   *
+   * @param nomElem
+   *    Le nom de l'élement structurel
+   * @param monde
+   *    Le monde auquel appartient l'élement structurel
+   *
+   * @see Monde
+  */
   public ElementStructurel(String nomElem,Monde monde){
     super(nomElem,monde);
   }
