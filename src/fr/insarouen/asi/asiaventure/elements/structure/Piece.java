8d411b716560356733e915025dafa0e3b1ef1800
Pineau Anna <anna.pineau@insa-rouen.fr>
2020-02-07 19:42:21 +0100

    Documentations sur la pièce fini

--- src/fr/insarouen/asi/asiaventure/elements/structure/Piece.java
+++ src/fr/insarouen/asi/asiaventure/elements/structure/Piece.java
@@ -95,36 +95,97 @@ public class Piece extends ElementStructurel {
 
 
 
+  /**
+   * Vérifies si la pièce a bien l'objet
+   *
+   *@param nomObjet, objet dont la présence est vérifiée
+   *
+   *@return true si l'objet est dans la pièce
+   */
   public boolean contientObjet(String nomObjet){
     return Utilitaire.contientEntite(nomObjet,this.tabObjet);
   }
 
 
+  /**
+   * Vérifies si la pièce a bien l'objet
+   *
+   *@param Objet, objet dont la présence est vérifiée
+   *@see Objet
+   *
+   *@return true si l'objet est dans la pièce
+   */
   public boolean contientObjet(Objet o){
     return contientObjet(o.getNom());
   }
 
+
+  /**
+   * Vérifies si le vivant est bien dans la pièce
+   *
+   *@param nomVivant, vivant dont la présence est vérifiée
+   *
+   *@return true si le vivant est dans la pièce
+   */
   public boolean contientVivant(String nomVivant){
     return Utilitaire.contientEntite(nomVivant,this.tabVivant);
   }
 
 
+  /**
+   * Vérifies si le vivant est bien dans la pièce
+   *
+   *@param v, vivant dont la présence est vérifiée
+   *@see Vivant
+   *
+   *@return true si le vivant est dans la pièce
+   */
   public boolean contientVivant(Vivant v){
     return contientVivant(v.getNom());
   }
 
+
+  /**
+   *Ajoute un objet dans la pièce
+   *
+   *@param obj, obj qui va être ajouté à la liste d'objets de la pièce
+   *@see Objet
+   *
+   */
   public void deposer(Objet obj){
     Utilitaire.ajouterEntite(obj,this.tabObjet);
   }
 
+  /**
+   *Ajoute un vivant dans la pièce
+   *
+   *@param v, vivant qui va être ajouté à la liste des vivants de la pièce
+   *@see Vivant
+   *
+   */
   public void entrer(Vivant v){
     Utilitaire.ajouterEntite(v,this.tabVivant);
   }
 
+
+
+  /**
+   * Obtient la liste des objets
+   *
+   * @return le tableau d'objets
+   */
   public Objet[] getObjets(){
     return this.tabObjet;
   }
 
+
+  /**
+   * Retourne la porte de la pièce dont le nom correspond
+   *
+   *@param nomPorte, le nom de la porte à retourner
+   *
+   * @return la porte
+   */
   public Porte getPorte(String nomPorte){
     for (Porte p : this.tabPorte) {
       if (p.getNom().equals(nomPorte)) {
@@ -134,22 +195,60 @@ public class Piece extends ElementStructurel {
     return null;
   }
 
+  /**
+   *Retire un objet dans la pièce et le retourne
+   *
+   *@param o, nom de l'objet qui va être retiré à la liste d'objets de la pièce
+   *
+   *@return objet
+   */
   public Objet retirer(String o){
     return (Objet) Utilitaire.retirerEntite(o,this.tabObjet);
   }
 
+  /**
+   *Retire un objet dans la pièce et le retourne
+   *
+   *@param o, objet qui va être retiré à la liste d'objets de la pièce
+   *@see objet
+   *
+   *@return objet
+   */
   public Objet retirer(Objet o){
    return retirer(o.getNom());
   }
 
+
+  /**
+   *Retire un vivant dans la pièce et le retourne
+   *
+   *@param v, nom du vivant qui va être retiré à la liste des vivants de la pièce
+   *
+   *@return Vivant
+   */
   public Vivant sortir(String v){
     return (Vivant) Utilitaire.retirerEntite(v,this.tabVivant);
   }
 
+  /**
+   *Retire un vivant dans la pièce et le retourne
+   *
+   *@param v, vivant qui va être retiré à la liste des vivants de la pièce
+   *@see Vivant
+   *
+   *@return Vivant
+   */
   public Vivant sortir(Vivant v){
     return sortir(v.getNom());
   }
 
+
+  /** Retourne sous forme de String les informations sur la pièce.
+   *Donne ainsi le nombre et l'ensemble des objets, le nombre et l'ensemble des
+   *vivants et le nombre et l'ensemble des portes. 
+   *
+   *
+   */
   public String toString(){
     StringBuilder EntiteStr = new StringBuilder();
 
@@ -165,7 +264,7 @@ public class Piece extends ElementStructurel {
     EntiteStr.append(Utilitaire.toStringTabEntite(this.tabVivant));
     EntiteStr.append("\n");
 
-    return String.format("La pièce %s \n %s", this.getNom(), EntiteStr);
+    return String.format("La pièce %s \n%s", this.getNom(), EntiteStr);
   }
 
 
